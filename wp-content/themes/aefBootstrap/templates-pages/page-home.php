2afdb8238fbdcafb8f99df1906e17de1770c5921
aefoley <ann@prairieopolis.com>
2020-04-14 17:03:58 -0500

    Did a test, getting ready to replace CSS and start themeing

--- wp-content/themes/aefBootstrap/templates-pages/page-home.php
+++ wp-content/themes/aefBootstrap/templates-pages/page-home.php
@@ -12,7 +12,7 @@ get_header(); ?>
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
         <div class="hero-unit">
-                <h1><?php the_title();?> FOO</h1>
+                <h1><?php the_title();?></h1>
                 <?php the_content(); ?>
         </div><!--/.hero-unit -->
 
